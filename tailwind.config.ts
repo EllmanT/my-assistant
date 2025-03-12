f790aabb018dfc729d4fdfac20c39b0a5585b9e4
EllmanT <tmuranda1@gmail.com>
2025-03-12 16:40:34 +0200

    Sped up letters fade in

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ export default {
   theme: {
     extend: {
       animation: {
-        letterPop: "letterPop 1s ease-in-out forwards", // Animation for the letter popping in
+        letterPop: "letterPop 0.5s ease-in-out forwards", // Animation for the letter popping in
       },
       keyframes: {
         letterPop: {
