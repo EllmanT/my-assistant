b4d5ff7819479f185a6610afdb59c9396ea4429e
EllmanT <tmuranda1@gmail.com>
2025-03-15 15:45:08 +0200

    Updated home transitions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ export default {
   theme: {
     extend: {
       animation: {
-        letterPop: "letterPop 3.0s ease-in-out forwards  infinite", // Animation for the letter popping in
+        letterPop: "letterPop 0.3s ease-in-out forwards ", // Animation for the letter popping in
       },
       keyframes: {
         letterPop: {
